7d46b0fc9bdc93ab3d6f82ce60c39d2c9a1bd705
John D'Oriocourt <kibner@gmail.com>
2021-09-10 18:01:36 -0500

    added function to return the maximum number of digits to allow in user prompt

--- 001-prime-numbers/main.c
+++ 001-prime-numbers/main.c
@@ -1,6 +1,22 @@
+//    START
+//      Step 1 → Take integer variable A
+//      Step 2 → Divide the variable A with (A-1 to 2)
+//      Step 3 → If A is divisible by any value (A-1 to 2) it is not prime
+//      Step 4 → Else it is prime
+//    STOP
 #include <stdio.h>
+#include <limits.h>
+#include <math.h>
+
+int get_max_input_length(void);
 
 int main(void) {
-    printf("Hello, World!\n");
+    const int MAX_INPUT_LENGTH = get_max_input_length();
+    printf("Enter a number to use as the ceiling when calculating prime numbers:  \n");
+
     return 0;
+}
+
+int get_max_input_length(void) {
+    return (int) log10(INT_MAX) + 1;
 }
