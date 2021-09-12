c12aee29a285ff3e3b083606707f9cbf7da046db
John D'Oriocourt <kibner@gmail.com>
2021-09-12 10:55:38 -0500

    simplified getting input from user

--- 001-prime-numbers/main.c
+++ 001-prime-numbers/main.c
@@ -4,59 +4,27 @@
 //      Step 3 → If A is divisible by any value (A-1 to 2) it is not prime
 //      Step 4 → Else it is prime
 //    STOP
-#include <stdio.h>
-#include <limits.h>
-#include <math.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
 
-unsigned int get_max_input_length(void);
+#define INPUT_FORMAT_STRING_LENGTH ((const unsigned int)(3))
 
-char *allocate_memory_for_string(unsigned int max_string_length);
-
-const char *get_input_format_string(unsigned int max_input_string_length);
+void set_input_format_string(char (*format_string)[INPUT_FORMAT_STRING_LENGTH], size_t buffer_size);
 
 void get_vscanf_input(const char *input_format_string, ...);
 
 int main(void) {
-    const unsigned int max_input_length = get_max_input_length();
-    const char *const input_format_string = get_input_format_string(max_input_length);
+    char input_format_string[INPUT_FORMAT_STRING_LENGTH];
     unsigned int prime_ceiling;
+    set_input_format_string(&input_format_string, sizeof(input_format_string));
     printf("Enter a number to use as the ceiling when calculating prime numbers:  \n");
     get_vscanf_input(input_format_string, &prime_ceiling);
 
     return 0;
 }
 
-unsigned int get_max_input_length(void) {
-    return (unsigned int) log10(UINT_MAX) + 1;
-}
-
-char *allocate_memory_for_string(const unsigned int max_string_length) {
-    unsigned int actual_length;
-
-    if (max_string_length == UINT_MAX) {
-        actual_length = max_string_length;
-    } else {
-        actual_length = max_string_length + 1;
-    }
-
-    return (char *) malloc(sizeof(char) * actual_length);
-}
-
-const char *get_input_format_string(const unsigned int max_input_string_length) {
-    char *const input_format_string = allocate_memory_for_string(max_input_string_length);
-
-    if (input_format_string == NULL) {
-        printf("Memory not allocated.\n");
-
-        return NULL;
-    } else {
-        snprintf(input_format_string, sizeof(char) * (max_input_string_length + 1), "%s%u%s", "%",
-                 max_input_string_length, "u");
-
-        return input_format_string;
-    }
+void set_input_format_string(char (*format_string)[INPUT_FORMAT_STRING_LENGTH], size_t buffer_size) {
+    snprintf(*format_string, buffer_size, "%s", "%u");
 }
 
 void get_vscanf_input(const char *const input_format_string, ...) {
