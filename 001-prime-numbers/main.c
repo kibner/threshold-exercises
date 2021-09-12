18a572e06e07e79925765b8d2851f278d4d29dc7
John D'Oriocourt <kibner@gmail.com>
2021-09-12 09:30:31 -0500

    able to get input from user

--- 001-prime-numbers/main.c
+++ 001-prime-numbers/main.c
@@ -7,16 +7,61 @@
 #include <stdio.h>
 #include <limits.h>
 #include <math.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-int get_max_input_length(void);
+unsigned int get_max_input_length(void);
+
+char *allocate_memory_for_string(unsigned int max_string_length);
+
+const char *get_input_format_string(unsigned int max_input_string_length);
+
+void get_vscanf_input(const char *input_format_string, ...);
 
 int main(void) {
-    const int MAX_INPUT_LENGTH = get_max_input_length();
+    const unsigned int max_input_length = get_max_input_length();
+    const char *const input_format_string = get_input_format_string(max_input_length);
+    unsigned int prime_ceiling;
     printf("Enter a number to use as the ceiling when calculating prime numbers:  \n");
+    get_vscanf_input(input_format_string, &prime_ceiling);
 
     return 0;
 }
 
-int get_max_input_length(void) {
-    return (int) log10(INT_MAX) + 1;
+unsigned int get_max_input_length(void) {
+    return (unsigned int) log10(UINT_MAX) + 1;
+}
+
+char *allocate_memory_for_string(const unsigned int max_string_length) {
+    unsigned int actual_length;
+
+    if (max_string_length == UINT_MAX) {
+        actual_length = max_string_length;
+    } else {
+        actual_length = max_string_length + 1;
+    }
+
+    return (char *) malloc(sizeof(char) * actual_length);
+}
+
+const char *get_input_format_string(const unsigned int max_input_string_length) {
+    char *const input_format_string = allocate_memory_for_string(max_input_string_length);
+
+    if (input_format_string == NULL) {
+        printf("Memory not allocated.\n");
+
+        return NULL;
+    } else {
+        snprintf(input_format_string, sizeof(char) * (max_input_string_length + 1), "%s%u%s", "%",
+                 max_input_string_length, "u");
+
+        return input_format_string;
+    }
+}
+
+void get_vscanf_input(const char *const input_format_string, ...) {
+    va_list ap;
+    va_start(ap, input_format_string);
+    vscanf(input_format_string, ap);
+    va_end(ap);
 }
