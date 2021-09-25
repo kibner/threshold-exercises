2c46631e949fa070aec5ba6398ac75851e575ccb
John D'Oriocourt <kibner@gmail.com>
2021-09-25 09:08:11 -0500

    - refactored function names - cleaned up logic of functions

--- 001-prime-numbers/main.c
+++ 001-prime-numbers/main.c
@@ -1,10 +1,3 @@
-//    START
-//      Step 1 → Take integer variable A
-//      Step 2 → Divide the variable A with (A-1 to 2)
-//      Step 3 → If A is divisible by any value (A-1 to 2) it is not prime
-//      Step 4 → Else it is prime
-//    STOP
-
 #include <errno.h>
 #include <inttypes.h>
 #include <iso646.h>
@@ -15,35 +8,57 @@
 
 #define INPUT_FORMAT_STRING_LENGTH ((const uint8_t)(11))
 
+uint32_t get_valid_prime_ceiling(void);
+
+bool is_valid_prime_ceiling(unsigned long ulong_value);
+
+uint32_t get_uint32_from_stream(FILE *stream, const char *prompt_message);
+
 bool has_fgets_erred(const char *fgets_result);
 
-uint32_t parse_ulong_string(const char *ulong_string);
-
-bool is_valid_ulong(unsigned long ulong_value);
-
-uint32_t get_ulong_from_stream(FILE *stream, const char *prompt_message);
+uint32_t parse_uint32_string(const char *ulong_string);
 
 int main(void) {
-    uint32_t prime_ceiling = get_ulong_from_stream(
-            stdin,
-            "Enter a number to use as the ceiling when calculating prime numbers:");
+    const uint32_t prime_ceiling = get_valid_prime_ceiling();
 
     printf("Prime number ceiling: %"PRIu32, prime_ceiling);
 
     return EXIT_SUCCESS;
 }
 
-uint32_t get_ulong_from_stream(FILE *stream, const char *prompt_message) {
-    uint32_t ulong_value;
-    bool is_success = false;
+uint32_t get_valid_prime_ceiling(void) {
+    uint32_t result;
 
-    while (is_success not_eq true) {
+    while (true) {
+        result = get_uint32_from_stream(
+                stdin,
+                "Enter a number to use as the ceiling when calculating prime numbers:");
+
+        if (not is_valid_prime_ceiling(result)) {
+            fprintf(stderr, "Not a valid number for the prime ceiling. Please try again.\n");
+
+            continue;
+        }
+
+        return result;
+    }
+}
+
+bool is_valid_prime_ceiling(const unsigned long ulong_value) {
+    return ulong_value >= 2;
+}
+
+uint32_t get_uint32_from_stream(FILE *stream, const char *prompt_message) {
+    uint32_t result;
+    char input_buffer[INPUT_FORMAT_STRING_LENGTH];
+    const char *fgets_result;
+
+    while (true) {
         if (prompt_message not_eq NULL) {
             printf("%s\n", prompt_message);
         }
 
-        char input_buffer[INPUT_FORMAT_STRING_LENGTH];
-        const char *fgets_result = fgets(input_buffer, INPUT_FORMAT_STRING_LENGTH, stream);
+        fgets_result = fgets(input_buffer, INPUT_FORMAT_STRING_LENGTH, stream);
 
         if (has_fgets_erred(fgets_result)) {
             fprintf(stderr, "Unknown error reading from stream.\n");
@@ -51,41 +66,37 @@ uint32_t get_ulong_from_stream(FILE *stream, const char *prompt_message) {
             continue;
         }
 
-        ulong_value = parse_ulong_string(input_buffer);
+        result = parse_uint32_string(input_buffer);
 
-        if (not is_valid_ulong(ulong_value)) {
-            fprintf(stderr, "Error validating input. Please try again.\n");
-
+        if (errno not_eq 0) {
             continue;
         }
 
-        is_success = true;
+        return result;
     }
-
-    return ulong_value;
 }
 
 bool has_fgets_erred(const char *fgets_result) {
     return fgets_result == NULL;
 }
 
-uint32_t parse_ulong_string(const char *ulong_string) {
+uint32_t parse_uint32_string(const char *ulong_string) {
     errno = 0;
-    const unsigned long parsed_number = strtoul(ulong_string, NULL, 0);
+    unsigned long parsed_number = strtoul(ulong_string, NULL, 0);
 
-    if (errno not_eq 0) {
-        if (errno == ERANGE) {
+    switch (errno) {
+        case 0:
+            // successfully parsed
+            break;
+        case ERANGE:
             fprintf(stderr, "Out of range error when parsing unsigned long from string.\n");
-        } else {
+            parsed_number = 0;
+            break;
+        default:
             fprintf(stderr, "Unknown error when parsing unsigned long from string.\n");
-        }
-
-        return 0;
+            parsed_number = 0;
+            break;
     }
 
     return (uint32_t) parsed_number;
-}
-
-bool is_valid_ulong(const unsigned long ulong_value) {
-    return ulong_value > 0;
 }
