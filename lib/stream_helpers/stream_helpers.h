b6f66ce1c637a1ec2c0b0d4be41a3eaf58c7a61a
John D'Oriocourt <kibner@gmail.com>
2021-09-26 13:35:51 -0500

    made parameters in header more const correct

--- lib/stream_helpers/stream_helpers.h
+++ lib/stream_helpers/stream_helpers.h
@@ -7,8 +7,8 @@
 
 uint32_t get_uint32_from_stream(FILE *const stream, const char *const prompt_message);
 
-bool has_fgets_erred(const char *fgets_result);
+bool has_fgets_erred(const char *const fgets_result);
 
-uint32_t parse_uint32_string(const char *uint32_string);
+uint32_t parse_uint32_string(const char *const uint32_string);
 
 #endif //THRESHOLD_EXERCISES_STREAM_HELPERS_H
