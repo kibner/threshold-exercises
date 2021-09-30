b82072538f6c9e1729e80978ad2135f27f1e2a11
John D'Oriocourt <kibner@gmail.com>
2021-09-30 17:11:13 -0500

    finished brute force calculation

--- 001-prime-numbers/calculations/brute_force.c
+++ 001-prime-numbers/calculations/brute_force.c
@@ -2,19 +2,43 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define FIRST_PRIME ((const uint32_t)(2))
+#define SECOND_PRIME ((const uint32_t)(3))
+
 void calculate_prime_numbers(const uint32_t upper_limit, prime_found_callback_type prime_found_callback) {
-    const uint32_t first_prime = 2;
-    const uint32_t second_prime = 3;
+    if (upper_limit >= FIRST_PRIME) {
+        prime_found_callback(FIRST_PRIME);
 
-    if (upper_limit >= first_prime) {
-        prime_found_callback(first_prime);
-
-        for (uint32_t test_value = second_prime; test_value <= upper_limit; test_value = test_value + 2) {
-            bool is_prime = true; // todo: test if test_value is a prime or not
-
-            if (is_prime) {
+        for (uint32_t test_value = SECOND_PRIME;
+             test_value <= upper_limit;
+             test_value = test_value + 2
+                ) {
+            if (is_prime(test_value)) {
                 prime_found_callback(test_value);
             }
         }
     }
+}
+
+bool is_prime(const uint32_t possible_prime) {
+    if (possible_prime < FIRST_PRIME) {
+        return false;
+    }
+
+    if (possible_prime == FIRST_PRIME) {
+        return true;
+    }
+
+    const uint32_t upper_limit = possible_prime / 2;
+
+    for (uint32_t test_value = SECOND_PRIME;
+         test_value <= upper_limit;
+         test_value = test_value + 2
+            ) {
+        if (possible_prime % test_value == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- 001-prime-numbers/calculations/brute_force.h
+++ 001-prime-numbers/calculations/brute_force.h
@@ -1,10 +1,13 @@
 #ifndef THRESHOLD_EXERCISES_BRUTE_FORCE_H
 #define THRESHOLD_EXERCISES_BRUTE_FORCE_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 typedef void (*const prime_found_callback_type)(const uint32_t);
 
 void calculate_prime_numbers(const uint32_t upper_limit, prime_found_callback_type prime_found_callback);
+
+bool is_prime(const uint32_t possible_prime);
 
 #endif //THRESHOLD_EXERCISES_BRUTE_FORCE_H
