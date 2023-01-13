40e3c9cd86487bcc1b5b815cd54514595b58f620
John D'Oriocourt <kibner@gmail.com>
2023-01-12 20:59:02 -0600

    applied fix suggested by clan-tidy

--- lib/stream_helpers/stream_helpers.h
+++ lib/stream_helpers/stream_helpers.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-uint32_t get_uint32_from_stream(FILE *const stream, const char *const prompt_message);
+uint32_t get_uint32_from_stream(FILE *stream, const char *prompt_message);
 
 #endif //THRESHOLD_EXERCISES_STREAM_HELPERS_H
